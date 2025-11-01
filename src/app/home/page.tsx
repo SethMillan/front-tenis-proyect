976462cb9bfb27b29618051ee2ed330e5c735a3d
Seth Millán <millanseth@gmail.com>
2025-11-01 17:48:05 -0600

    feat(home): :sparkles: teniscard added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import CardTenis from "../components/CardTenis";
 
 const page = () => {
   return (
-    <div>jiji</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center content-center h-full ">
+      <CardTenis
+        nombre={""}
+        color={""}
+        categoria={""}
+        marca={""}
+        url_imagen={""}
+      />
+    </div>
+  );
+};
 
-export default page
+export default page;
