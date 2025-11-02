f5f7b78260350fe82c22aa56388b40840ce3a02c
AdrianMO <adrian.martinez.260821@gmail.com>
2025-11-02 16:37:15 -0600

    feat(Sales): :construction: Se setearon los datos de las ventas en la tabla

--- src/app/components/TableSales.tsx
+++ src/app/components/TableSales.tsx
@@ -1,14 +1,26 @@
+'use client'
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+  Table,TableBody,TableCaption,TableCell,TableHead,TableHeader,TableRow,
 } from "@/components/ui/table"
+import { useEffect,useState } from "react"
 
 export function TableSales() {
+    const [sales, setSales] = useState([]);
+    useEffect(() =>{
+        async function fetchSalesData(){
+            try{
+                const response = await fetch("https://back-tenis-proyect.onrender.com/ventas");
+                const data = await response.json();
+                console.log("Fetched sales data:", data);
+                setSales(data);
+            }catch (error) {
+                console.error("Error fetching sales data:", error);
+            }
+        }
+        fetchSalesData();
+    },[]);
+
+
   return (
     <Table>
       <TableHeader>
@@ -23,7 +35,17 @@ export function TableSales() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {/* Aquí mapeas tus datos, por ahora vacío */}
+        {sales.map((venta: any) => (
+          <TableRow key={venta.id}>
+            <TableCell>{venta.id}</TableCell>
+            <TableCell>{venta.fecha}</TableCell>
+            <TableCell>{venta.tipo_pago}</TableCell>
+            <TableCell>{venta.total}</TableCell>
+            <TableCell>{venta.clientes.nombres}</TableCell>
+            <TableCell>{venta.empleados.nombre}</TableCell>
+            <TableCell>${venta.total}</TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   )
