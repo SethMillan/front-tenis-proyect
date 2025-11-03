a766c43a0e3d43619d5420589302f9116f4c859a
AdrianMO <adrian.martinez.260821@gmail.com>
2025-11-02 18:58:10 -0600

    refactor(Sales): :construction: Tipado de objeto ventas

--- src/components/TableSales.tsx
+++ src/components/TableSales.tsx
@@ -4,14 +4,16 @@ import {
 } from "@/components/ui/table"
 import { useEffect,useState } from "react"
 import { API_URL } from "@/features/shared/api-url"
+import { Venta, Cliente, Empleado } from "@/types/types"
+
 
 export function TableSales() {
-    const [sales, setSales] = useState([]);
+    const [sales, setSales] = useState<Venta[]>([]);
     useEffect(() =>{
         async function fetchSalesData(){
             try{
                 const response = await fetch(`${API_URL}/ventas`);
-                const data = await response.json();
+                const data: Venta[] = await response.json();
                 console.log("Fetched sales data:", data);
                 setSales(data);
             }catch (error) {
@@ -20,14 +22,23 @@ export function TableSales() {
         }
         fetchSalesData();
     },[]);
-    function formatDate(dateString:string) {
-  const date = new Date(dateString);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-  
-  return `${day}/${month}/${year}`;
-}
+  function formatDate(dateString:string) {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    
+    return `${day}/${month}/${year}`;
+  }
+   const formatFullName = (person?: Cliente | Empleado | null) => {
+      if (!person) return 'N/A';
+      // clientes usan 'nombres', empleados usan 'nombre'
+      const first = ('nombres' in person) ? (person as Cliente).nombres : (person as Empleado).nombre;
+      const apellidoP = person.apellido_p ?? '';
+      const apellidoM = person.apellido_m ?? '';
+      return `${first} ${apellidoP} ${apellidoM}`.trim();
+    };
+
 
 
   return (
@@ -50,8 +61,8 @@ export function TableSales() {
             <TableCell>{formatDate(venta.fecha)}</TableCell>
             <TableCell>{venta.tipo_pago}</TableCell>
             <TableCell>{venta.total}</TableCell>
-            <TableCell>{venta.clientes.nombres}</TableCell>
-            <TableCell>{venta.empleados.nombre}</TableCell>
+            <TableCell>{formatFullName(venta.clientes)}</TableCell>
+            <TableCell>{formatFullName(venta.empleados)}</TableCell>
             <TableCell>${venta.total}</TableCell>
           </TableRow>
         ))}
